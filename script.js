5767dd5b16eaa5f9695cc5b8b18a26198ca72c30
kangheechan <heechan30927000@gmail.com>
2025-02-04 23:21:57 +0900

    added overlay and pop-up when hovered

--- script.js
+++ script.js
@@ -1,12 +1,71 @@
-const svgContainer = document.getElementById('svg-container');
-const plusBtn = document.getElementById("plus-btn");
-const minusBtn = document.getElementById("minus-btn");
+const svgContainer = document.querySelector(".svg-container");
+const plusBtn = document.querySelector(".plus-btn");
+const minusBtn = document.querySelector(".minus-btn");
 const koreaPath = document.querySelector(".Korea");
 const indonesiaPaths = document.querySelectorAll(".Indonesia");
 const australiaPaths = document.querySelectorAll(".Australia");
+const overlay = document.querySelector(".overlay");
+const title = document.querySelector(".title");
+const description = document.querySelector(".description");
+const image = document.querySelector(".image");
+
 
 // blink starts when page is loaded
 plusBtn.classList.add("blinking");
+
+// turns off overlay and pop-up
+function overlayOff() {
+    overlay.style.display = "none";
+}
+
+// opens korea information pop-up
+function koreaInfo() {
+    overlay.style.display = "flex";
+}
+
+// opens indonesia information pop-up
+function indonesiaInfo() {
+    overlay.style.display = "flex";
+}
+
+// opens australia information pop-up
+function australiaInfo() {
+    overlay.style.display = "flex";
+}
+
+// blinking countries when plus button is clicked
+function addBlinking() {
+    koreaPath.classList.add("blinking");
+    indonesiaPaths.forEach(path => path.classList.add("blinking"));
+    australiaPaths.forEach(path => path.classList.add("blinking"));
+}
+
+// remove blinking countries when minus button is clicked
+function removeBlinking() {
+    koreaPath.classList.remove("blinking");
+    indonesiaPaths.forEach(path => path.classList.remove("blinking"));
+    australiaPaths.forEach(path => path.classList.remove("blinking"));
+}
+
+// info is shown and background is blurred when hovered, returns when not hovered
+function addEventListeners() {
+    koreaPath.addEventListener("mouseover", koreaInfo);
+    indonesiaPaths.forEach(path => path.addEventListener("mouseover", indonesiaInfo));
+    australiaPaths.forEach(path => path.addEventListener("mouseover", australiaInfo));
+    koreaPath.addEventListener("mouseout", overlayOff);
+    indonesiaPaths.forEach(path => path.addEventListener("mouseout", overlayOff));
+    australiaPaths.forEach(path => path.addEventListener("mouseout", overlayOff));
+}
+
+// removed event listeners when minus button is clicked
+function removeEventListeners() {
+    koreaPath.removeEventListener("mouseover", koreaInfo);
+    indonesiaPaths.forEach(path => path.removeEventListener("mouseover", indonesiaInfo));
+    australiaPaths.forEach(path => path.removeEventListener("mouseover", australiaInfo));
+    koreaPath.removeEventListener("mouseout", overlayOff);
+    indonesiaPaths.forEach(path => path.removeEventListener("mouseout", overlayOff));
+    australiaPaths.forEach(path => path.removeEventListener("mouseout", overlayOff));
+}
 
 // zoom in function when plus button is clicked
 function zoomIn() {
@@ -14,24 +73,8 @@ function zoomIn() {
     svgContainer.style.transform = "scale(1.6)";
     plusBtn.classList.remove("blinking");
     minusBtn.classList.add("blinking");
-
-    // blinking countries when plus button is clicked
-    koreaPath.classList.add("blinking");
-    indonesiaPaths.forEach(path => {
-        path.classList.add("blinking");
-    })
-    australiaPaths.forEach(path => {
-        path.classList.add("blinking");
-    })
-
-    // info is shown when corresponding country is hovered
-    koreaPath.addEventListener("mouseover", koreaInfo);
-    indonesiaPaths.forEach(path => {
-        path.addEventListener("mouseover", indonesiaInfo);
-    });
-    australiaPaths.forEach(path => {
-        path.addEventListener("mouseover", australiaInfo);
-    });
+    addBlinking();
+    addEventListeners();
 }
 
 // zoom out function when minus button is clicked
@@ -39,25 +82,8 @@ function zoomOut() {
     svgContainer.style.transform = "scale(1.0)";
     minusBtn.classList.remove("blinking");
     plusBtn.classList.add("blinking");
-
-    // remove blinking countries when minus button is clicked
-    koreaPath.classList.remove("blinking");
-    indonesiaPaths.forEach(path => {
-        path.classList.remove("blinking");
-    })
-    australiaPaths.forEach(path => {
-        path.classList.remove("blinking");
-    })
-}
-
-
-function koreaInfo() {
-}
-
-function indonesiaInfo() {
-}
-
-function australiaInfo() {
+    removeBlinking();
+    removeEventListeners();
 }
 
 plusBtn.addEventListener("click", zoomIn);

ed2b06ce712b69f78fd0c2952c14d6e4f8920950
heechan <101608118+heechan2003@users.noreply.github.com>
2025-02-05 21:57:05 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -5,13 +5,25 @@ const koreaPath = document.querySelector(".Korea");
 const indonesiaPaths = document.querySelectorAll(".Indonesia");
 const australiaPaths = document.querySelectorAll(".Australia");
 const overlay = document.querySelector(".overlay");
-const title = document.querySelector(".title");
+const box = document.querySelector(".box");
+const title = document.querySelector(".box-title");
 const description = document.querySelector(".description");
 const image = document.querySelector(".image");
+const caption = document.querySelector(".caption");
 
 
 // blink starts when page is loaded
 plusBtn.classList.add("blinking");
+
+// displays and update contents for pop-up box
+function displayPopup(countryName, imageSrc, captionTxt, text) {
+    title.textContent = countryName;
+    image.src = imageSrc;
+    image.alt = countryName;
+    caption.textContent = captionTxt;
+    description.innerHTML = text;
+    overlay.style.display = "flex";
+}
 
 // turns off overlay and pop-up
 function overlayOff() {
@@ -20,17 +32,20 @@ function overlayOff() {
 
 // opens korea information pop-up
 function koreaInfo() {
-    overlay.style.display = "flex";
+    displayPopup("Korea", "assets/korea.jpg", "Comrades", "Born and raised until I turned 9<br>Returned to Korea after turning 16<br>Finished one and a half year military service<br><br><h1 style='font-size: 30px;'>Favorite Game</h1><ul style='font-size: 20px;'><li>League of Legends</li><li>League of Legends</li><li>League of Legends</li></ul>");
+    box.style.backgroundColor = "rgb(59, 77, 115)"
 }
 
 // opens indonesia information pop-up
 function indonesiaInfo() {
-    overlay.style.display = "flex";
+    displayPopup("Indonesia", "assets/indonesia.jpg", "When I was 10", "Moved at 9 because of my father's work<br>Went to an international school<br>Probably the best days of my life<br><br><h1 style='font-size: 30px;'>Favorite Food</h1><ul style='font-size: 20px;'><li>Sushi</li><li>Budae Jjigae(Korean stew)</li><li>Sate</li></ul>");
+    box.style.backgroundColor = "rgb(229, 122, 102)";
 }
 
 // opens australia information pop-up
 function australiaInfo() {
-    overlay.style.display = "flex";
+    displayPopup("Australia", "", "aus", "Name: Kang Heechan<br>Age: 21<br>MBTI: ISTJ<br>Degree: CS<br>Year: 2nd year<br><br> Couldn't really make any friends during my first year... I hope I can make lasting friends in my second year. :)");
+    box.style.backgroundColor = "rgb(217, 179, 130)";
 }
 
 // blinking countries when plus button is clicked
